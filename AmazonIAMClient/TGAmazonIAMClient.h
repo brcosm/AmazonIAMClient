d0aeaf6d01560267444a49519c17f2ce2d50e7f6
Brandon Smith <brcosm@gmail.com>
2013-01-19 19:17:58 -0800

    Added more of the IAM API

--- AmazonIAMClient/TGAmazonIAMClient.h
+++ AmazonIAMClient/TGAmazonIAMClient.h
@@ -15,8 +15,11 @@
 - (id)initWithAccessKeyID:(NSString *)accessKey
                    secret:(NSString *)secret;
 
+#pragma mark - Account API
+
 - (void)getAccountSummaryWithSuccess:(void (^)(id responseObject))success
                              failure:(void (^)(NSError *error))failure;
+#pragma mark - Group API
 
 - (void)addUser:(NSString *)userName
         toGroup:(NSString *)groupName
@@ -28,27 +31,113 @@
             success:(void (^)(id responseObject))success
             failure:(void (^)(NSError *error))failure;
 
+- (void)deleteGroup:(NSString *)groupName
+            success:(void (^)(id responseObject))success
+            failure:(void (^)(NSError *error))failure;
+
+- (void)getGroup:(NSString *)groupName
+          marker:(NSString *)marker
+        maxItems:(NSUInteger)maxItems
+         success:(void (^)(id responseObject))success
+         failure:(void (^)(NSError *error))failure;
+
+- (void)listGroupsWithPrefix:(NSString *)pathPrefix
+                      marker:(NSString *)marker
+                    maxItems:(NSUInteger)maxItems
+                     Success:(void (^)(id responseObject))success
+                     failure:(void (^)(NSError *error))failure;
+
+- (void)listGroupsForUser:(NSString *)userName
+                   marker:(NSString *)marker
+                 maxItems:(NSUInteger)maxItems
+                  Success:(void (^)(id responseObject))success
+                  failure:(void (^)(NSError *error))failure;
+
+- (void)removeUser:(NSString *)userName
+         fromGroup:(NSString *)groupName
+           success:(void (^)(id responseObject))success
+           failure:(void (^)(NSError *error))failure;
+
+#pragma mark - Role API
+
 - (void)createRole:(NSString *)roleName
               path:(NSString *)path
             policy:(NSDictionary *)policyInfo
            success:(void (^)(id responseObject))success
            failure:(void (^)(NSError *error))failure;
 
+- (void)deleteRole:(NSString *)roleName
+           success:(void (^)(id responseObject))success
+           failure:(void (^)(NSError *error))failure;
+
+- (void)getRole:(NSString *)roleName
+        success:(void (^)(id responseObject))success
+        failure:(void (^)(NSError *error))failure;
+
+#pragma mark - User API
+
 - (void)createUser:(NSString *)userName
               path:(NSString *)path
            success:(void (^)(id responseObject))success
            failure:(void (^)(NSError *error))failure;
 
-- (void)deleteGroup:(NSString *)groupName
-            success:(void (^)(id responseObject))success
-            failure:(void (^)(NSError *error))failure;
+- (void)deleteUser:(NSString *)userName
+           success:(void (^)(id responseObject))success
+           failure:(void (^)(NSError *error))failure;
+
+- (void)getUser:(NSString *)userName
+        success:(void (^)(id responseObject))success
+        failure:(void (^)(NSError *error))failure;
+
+- (void)listUsersWithPrefix:(NSString *)pathPrefix
+                     marker:(NSString *)marker
+                   maxItems:(NSUInteger)maxItems
+                    success:(void (^)(id responseObject))success
+                    failure:(void (^)(NSError *error))failure;
+
+#pragma mark - Policy API
 
 - (void)deletePolicy:(NSString *)policyName
            fromGroup:(NSString *)groupName
              success:(void (^)(id responseObject))success
              failure:(void (^)(NSError *error))failure;
 
-- (void)deleteRole:(NSString *)roleName
-           success:(void (^)(id responseObject))success
-           failure:(void (^)(NSError *error))failure;
+- (void)deletePolicy:(NSString *)policyName
+            fromRole:(NSString *)roleName
+             success:(void (^)(id responseObject))success
+             failure:(void (^)(NSError *error))failure;
+
+- (void)getPolicy:(NSString *)policyName
+         forGroup:(NSString *)groupName
+          success:(void (^)(id responseObject))success
+          failure:(void (^)(NSError *error))failure;
+
+- (void)getPolicy:(NSString *)policyName
+          forRole:(NSString *)roleName
+          success:(void (^)(id responseObject))success
+          failure:(void (^)(NSError *error))failure;
+
+- (void)getPolicy:(NSString *)policyName
+          forUser:(NSString *)userName
+          success:(void (^)(id responseObject))success
+          failure:(void (^)(NSError *error))failure;
+
+- (void)listRolesWithPrefix:(NSString *)pathPrefix
+                     marker:(NSString *)marker
+                   maxItems:(NSUInteger)maxItems
+                    success:(void (^)(id responseObject))success
+                    failure:(void (^)(NSError *error))failure;
+
+- (void)applyPolicy:(NSString *)policyName
+            toGroup:(NSString *)groupName
+             policy:(NSDictionary *)policyInfo
+            success:(void (^)(id responseObject))success
+            failure:(void (^)(NSError *error))failure;
+
+- (void)applyPolicy:(NSString *)policyName
+             toUser:(NSString *)userName
+             policy:(NSDictionary *)policyInfo
+            success:(void (^)(id responseObject))success
+            failure:(void (^)(NSError *error))failure;
+
 @end
--- AmazonIAMClient/TGAmazonIAMClient.m
+++ AmazonIAMClient/TGAmazonIAMClient.m
@@ -81,7 +81,7 @@ NSString * const kTGAmazonIAMDefaulVersion = @"2010-05-08";
     [self enqueueHTTPRequestOperation:requestOperation];
 }
 
-#pragma mark - API
+#pragma mark - Account API
 
 - (void)getAccountSummaryWithSuccess:(void (^)(id responseObject))success
                              failure:(void (^)(NSError *error))failure
@@ -90,14 +90,14 @@ NSString * const kTGAmazonIAMDefaulVersion = @"2010-05-08";
     [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
 }
 
+#pragma mark - Group API
+
 - (void)addUser:(NSString *)userName
         toGroup:(NSString *)groupName
         success:(void (^)(id responseObject))success
         failure:(void (^)(NSError *error))failure
 {
-    NSDictionary *params = @{ @"Action"    : @"AddUserToGroup",
-                              @"GroupName" : groupName,
-                              @"UserName"  : userName };
+    NSDictionary *params = @{ @"Action" : @"AddUserToGroup", @"GroupName" : groupName, @"UserName" : userName };
     [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
 }
 
@@ -107,11 +107,68 @@ NSString * const kTGAmazonIAMDefaulVersion = @"2010-05-08";
             failure:(void (^)(NSError *error))failure
 {
     NSString *defaultPath = path ? path : @"/";
-    NSDictionary *params = @{ @"Action"    : @"CreateGroup",
-                              @"GroupName" : groupName,
-                              @"Path"      : defaultPath };
+    NSDictionary *params = @{ @"Action" : @"CreateGroup", @"GroupName" : groupName, @"Path" : defaultPath };
     [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
 }
+
+- (void)deleteGroup:(NSString *)groupName
+            success:(void (^)(id responseObject))success
+            failure:(void (^)(NSError *error))failure
+{
+    NSDictionary *params = @{ @"Action" : @"DeleteGroup", @"GroupName" : groupName };
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+- (void)getGroup:(NSString *)groupName
+          marker:(NSString *)marker
+        maxItems:(NSUInteger)maxItems
+         success:(void (^)(id responseObject))success
+         failure:(void (^)(NSError *error))failure
+{
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    [params addEntriesFromDictionary:@{ @"Action" : @"GetGroup", @"GroupName" : groupName }];
+    if (marker) [params addEntriesFromDictionary:@{ @"Marker" : marker }];
+    if (maxItems) [params addEntriesFromDictionary:@{ @"MaxItems" : [NSString stringWithFormat:@"%u", maxItems] }];
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+- (void)listGroupsWithPrefix:(NSString *)pathPrefix
+                      marker:(NSString *)marker
+                    maxItems:(NSUInteger)maxItems
+                     Success:(void (^)(id responseObject))success
+                     failure:(void (^)(NSError *error))failure
+{
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    NSString *prefix = pathPrefix ? pathPrefix : @"/";
+    [params addEntriesFromDictionary:@{ @"Action" : @"ListGroups", @"PathPrefix" : prefix }];
+    if (marker) [params addEntriesFromDictionary:@{ @"Marker" : marker }];
+    if (maxItems) [params addEntriesFromDictionary:@{ @"MaxItems" : [NSString stringWithFormat:@"%u", maxItems] }];
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+- (void)listGroupsForUser:(NSString *)userName
+                   marker:(NSString *)marker
+                 maxItems:(NSUInteger)maxItems
+                  Success:(void (^)(id responseObject))success
+                  failure:(void (^)(NSError *error))failure
+{
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    [params addEntriesFromDictionary:@{ @"Action" : @"ListGroupsForUser", @"UserName" : userName }];
+    if (marker) [params addEntriesFromDictionary:@{ @"Marker" : marker }];
+    if (maxItems) [params addEntriesFromDictionary:@{ @"MaxItems" : [NSString stringWithFormat:@"%u", maxItems] }];
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+- (void)removeUser:(NSString *)userName
+         fromGroup:(NSString *)groupName
+           success:(void (^)(id responseObject))success
+           failure:(void (^)(NSError *error))failure
+{
+    NSDictionary *params = @{ @"Action" : @"RemoveUserFromGroup", @"GroupName" : groupName, @"UserName" : userName };
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+#pragma mark - Role API
 
 - (void)createRole:(NSString *)roleName
               path:(NSString *)path
@@ -122,42 +179,7 @@ NSString * const kTGAmazonIAMDefaulVersion = @"2010-05-08";
     NSString *defaultPath = path ? path : @"/";
     NSData *policyData = [NSJSONSerialization dataWithJSONObject:policyInfo options:0 error:nil];
     NSString *policyString = [[NSString alloc] initWithData:policyData encoding:NSUTF8StringEncoding];
-    NSDictionary *params = @{ @"Action"                   : @"CreateRole",
-                              @"RoleName"                 : roleName,
-                              @"Path"                     : defaultPath,
-                              @"AssumeRolePolicyDocument" : policyString };
-    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
-}
-
-- (void)createUser:(NSString *)userName
-              path:(NSString *)path
-           success:(void (^)(id responseObject))success
-           failure:(void (^)(NSError *error))failure
-{
-    NSString *defaultPath = path ? path : @"/";
-    NSDictionary *params = @{ @"Action"   : @"CreateUser",
-                              @"UserName" : userName,
-                              @"Path"     : defaultPath };
-    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
-}
-
-- (void)deleteGroup:(NSString *)groupName
-            success:(void (^)(id responseObject))success
-            failure:(void (^)(NSError *error))failure
-{
-    NSDictionary *params = @{ @"Action" : @"DeleteGroup",
-                              @"GroupName" : groupName };
-    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
-}
-
-- (void)deletePolicy:(NSString *)policyName
-           fromGroup:(NSString *)groupName
-             success:(void (^)(id responseObject))success
-             failure:(void (^)(NSError *error))failure
-{
-    NSDictionary *params = @{ @"Action" : @"DelegeGroupPolicy",
-                              @"GroupName" : groupName,
-                              @"PolicyName" : policyName };
+    NSDictionary *params = @{ @"Action" : @"CreateRole", @"RoleName" : roleName, @"Path" : defaultPath, @"AssumeRolePolicyDocument" : policyString };
     [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
 }
 
@@ -165,8 +187,142 @@ NSString * const kTGAmazonIAMDefaulVersion = @"2010-05-08";
            success:(void (^)(id responseObject))success
            failure:(void (^)(NSError *error))failure
 {
-    NSDictionary *params = @{ @"Action"   : @"DeleteRole",
-                              @"RoleName" : roleName };
+    NSDictionary *params = @{ @"Action" : @"DeleteRole", @"RoleName" : roleName };
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+- (void)getRole:(NSString *)roleName
+        success:(void (^)(id responseObject))success
+        failure:(void (^)(NSError *error))failure
+{
+    NSDictionary *params = @{ @"Action" : @"GetRole", @"RoleName" : roleName };
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+#pragma mark - User API
+
+- (void)createUser:(NSString *)userName
+              path:(NSString *)path
+           success:(void (^)(id responseObject))success
+           failure:(void (^)(NSError *error))failure
+{
+    NSString *defaultPath = path ? path : @"/";
+    NSDictionary *params = @{ @"Action" : @"CreateUser", @"UserName" : userName, @"Path" : defaultPath };
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+- (void)deleteUser:(NSString *)userName
+           success:(void (^)(id responseObject))success
+           failure:(void (^)(NSError *error))failure
+{
+    NSDictionary *params = @{ @"Action" : @"DeleteUser", @"UserName" : userName };
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+- (void)getUser:(NSString *)userName
+        success:(void (^)(id responseObject))success
+        failure:(void (^)(NSError *error))failure
+{
+    NSDictionary *params = @{ @"Action" : @"GetUser", @"UserName" : userName };
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+- (void)listUsersWithPrefix:(NSString *)pathPrefix
+                     marker:(NSString *)marker
+                   maxItems:(NSUInteger)maxItems
+                    success:(void (^)(id responseObject))success
+                    failure:(void (^)(NSError *error))failure
+{
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    NSString *prefix = pathPrefix ? pathPrefix : @"/";
+    [params addEntriesFromDictionary:@{ @"Action" : @"ListUsers", @"PathPrefix" : prefix }];
+    if (marker) [params addEntriesFromDictionary:@{ @"Marker" : marker }];
+    if (maxItems) [params addEntriesFromDictionary:@{ @"MaxItems" : [NSString stringWithFormat:@"%u", maxItems] }];
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+#pragma mark - Policy API
+
+- (void)deletePolicy:(NSString *)policyName
+           fromGroup:(NSString *)groupName
+             success:(void (^)(id responseObject))success
+             failure:(void (^)(NSError *error))failure
+{
+    NSDictionary *params = @{ @"Action" : @"DelegeGroupPolicy", @"GroupName" : groupName, @"PolicyName" : policyName };
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+- (void)deletePolicy:(NSString *)policyName
+            fromRole:(NSString *)roleName
+             success:(void (^)(id responseObject))success
+             failure:(void (^)(NSError *error))failure
+{
+    NSDictionary *params = @{ @"Action" : @"DeleteRolePolicy", @"RoleName" : roleName, @"PolicyName" : policyName };
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+- (void)getPolicy:(NSString *)policyName
+         forGroup:(NSString *)groupName
+          success:(void (^)(id responseObject))success
+          failure:(void (^)(NSError *error))failure
+{
+    NSDictionary *params = @{ @"Action" : @"GetGroupPolicy", @"GroupName" : groupName, @"PolicyName" : policyName };
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+- (void)getPolicy:(NSString *)policyName
+          forRole:(NSString *)roleName
+          success:(void (^)(id responseObject))success
+          failure:(void (^)(NSError *error))failure
+{
+    NSDictionary *params = @{ @"Action" : @"GetRolePolicy", @"RoleName" : roleName, @"PolicyName" : policyName };
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+- (void)getPolicy:(NSString *)policyName
+          forUser:(NSString *)userName
+          success:(void (^)(id responseObject))success
+          failure:(void (^)(NSError *error))failure
+{
+    NSDictionary *params = @{ @"Action" : @"GetUserPolicy", @"UserName" : userName, @"PolicyName" : policyName };
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+- (void)listRolesWithPrefix:(NSString *)pathPrefix
+                     marker:(NSString *)marker
+                   maxItems:(NSUInteger)maxItems
+                    success:(void (^)(id responseObject))success
+                    failure:(void (^)(NSError *error))failure
+{
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    NSString *prefix = pathPrefix ? pathPrefix : @"/";
+    [params addEntriesFromDictionary:@{ @"Action" : @"ListRoles", @"PathPrefix" : prefix }];
+    if (marker) [params addEntriesFromDictionary:@{ @"Marker" : marker }];
+    if (maxItems) [params addEntriesFromDictionary:@{ @"MaxItems" : [NSString stringWithFormat:@"%u", maxItems] }];
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+- (void)applyPolicy:(NSString *)policyName
+            toGroup:(NSString *)groupName
+             policy:(NSDictionary *)policyInfo
+            success:(void (^)(id responseObject))success
+            failure:(void (^)(NSError *error))failure
+{
+    NSData *policyData = [NSJSONSerialization dataWithJSONObject:policyInfo options:0 error:nil];
+    NSString *policyString = [[NSString alloc] initWithData:policyData encoding:NSUTF8StringEncoding];
+    NSDictionary *params = @{ @"Action" : @"PutGroupPolicy", @"PolicyName" : policyName, @"GroupName" : groupName, @"PolicyDocument" : policyString };
+    [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
+}
+
+- (void)applyPolicy:(NSString *)policyName
+             toUser:(NSString *)userName
+             policy:(NSDictionary *)policyInfo
+            success:(void (^)(id responseObject))success
+            failure:(void (^)(NSError *error))failure
+{
+    NSData *policyData = [NSJSONSerialization dataWithJSONObject:policyInfo options:0 error:nil];
+    NSString *policyString = [[NSString alloc] initWithData:policyData encoding:NSUTF8StringEncoding];
+    NSDictionary *params = @{ @"Action" : @"PutUserPolicy", @"PolicyName" : policyName, @"UserName" : userName, @"PolicyDocument" : policyString };
     [self enqueueIAMRequestWithMethod:@"GET" path:@"/" parameters:params success:success failure:failure];
 }
 
